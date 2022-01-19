aeac63c85b6b8d7982c2f4a9302d3a1121c0d993
Christoffer Brandt <christoffer.brant@hotmail.com>
2022-01-19 22:25:01 +0100

    feat: AVG, MIN, MAX functions added for displaying grades per course

--- HighSchool - Display Table.sql
+++ HighSchool - Display Table.sql
@@ -1,18 +1,29 @@
---select (E_Förnamn + char(9) + E_Efternamn) as Elev, (K_KursNamn) as Kurs, (EK_Betyg) as Betyg from tblEleverKurser
---join tblElever on E_ID = EK_ElevID
---join tblKurser on K_ID = EK_KursID
---where K_KursNamn = 'Matematik 1c'
+--Use HighSchool
+
+--select (P_Förnamn + char(9) + P_Efternamn) as 'Lärare' from tblPersonal_
+--where P_Arbete = 'Lärare'
+
+--select (E_Förnamn + char(9) + E_Efternamn) as 'Elev' from tblElever
 --order by E_Efternamn asc
 
-select (P_Förnamn + char(9) + P_Efternamn) as Lärare from tblPersonal_
-where P_Arbete = 'Lärare'
+--select (E_Förnamn + char(9) + E_Efternamn) as 'Elev', Kl_KlassNamn as 'Klass' from tblElever
+--join tblKlasser on Kl_ID = E_Klass_ID 
+--where E_Klass_ID = 1
 
-select (E_Förnamn + char(9) + E_Efternamn) as Elev from tblElever
-order by E_Efternamn asc
+--select count(EK_Betyg) as 'Nya betyg i månaden' from tblEleverKurser
+--where DATEDIFF(DAY, EK_BetygDatum, GETDATE()) < 31;
 
-select (E_Förnamn + char(9) + E_Efternamn) as Elev, Kl_KlassNamn as Klass from tblElever
-join tblKlasser on Kl_ID = E_Klass_ID 
-where E_Klass_ID = 1
 
-select count(EK_Betyg) as 'Nya betyg i månaden' from tblEleverKurser
-where DATEDIFF(DAY, EK_BetygDatum, GETDATE()) < 31
+--create view MedelBetyg
+--as
+--select AVG(B_BetygVärde) as 'Medelbetyg' from tblEleverKurser
+--join baseGiltigaBetyg on B_BetygBokstav = EK_Betyg
+
+select (K_KursNamn) as Kurs, 
+AVG(B_BetygVärde) as 'Medelbetyg',
+Min(B_BetygVärde) as 'Lägsta betyg',
+Max(B_BetygVärde) as 'Högsta betyg' from tblEleverKurser
+join baseGiltigaBetyg on B_BetygBokstav = EK_Betyg or B_BetygBokstav = null
+join tblKurser on K_ID = EK_KursID
+group by K_KursNamn
+order by K_KursNamn
