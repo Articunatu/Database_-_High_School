ba419789cd593e9d800be16815ab1331810a3ffe
Christoffer Brandt <christoffer.brant@hotmail.com>
2022-02-04 18:26:46 +0100

    feat: Tables for teacher department and their average salaries

--- HighSchool - Display Table.sql
+++ HighSchool - Display Table.sql
@@ -1,184 +1,204 @@
 
-Use HighSchool
+Use HighSchoolDB
 
-select (P_Förnamn + char(9) + P_Efternamn) as 'Personal', P_Arbete as 'Arbetsroll' from tblPersonal_
+drop proc spElevsAvslutadeKurser, spSkapaElever, spUtvaldPersonal
+alter database HighSchool modify name = HighSchoolDB
+
+select (E_FirstName + char(9) + E_LastName) as 'Personal', E_Job as 'Arbetsroll' from tblEmployees
 --where P_Arbete = 'Lärare'
 
-select * from tblKlasser
+select * from tblClasses
 
-select (E_Förnamn + char(9) + E_Efternamn) as 'Elev' from tblElever
-order by E_Efternamn asc
+select (S_FirstName + char(9) + S_LastName) as 'Elev' from tblStudents
+order by S_LastName asc
 
-select (E_Förnamn + char(9) + E_Efternamn) as 'Elev', Kl_KlassNamn as 'Klass' from tblElever
-join tblKlasser on Kl_ID = E_Klass_ID 
-where KL_ID = 1
+select (S_FirstName + char(9) + S_LastName) as 'Elev', Cl_Name as 'Klass' from tblStudents
+join tblClasses on Cl_ID = S_ClassID 
+where Cl_ID = 1
 
-select count(EK_Betyg) as 'Nya betyg denna månad' from tblEleverKurser ---Should be 2 grades with current table values
-where DATEDIFF(DAY, EK_BetygDatum, GETDATE()) <= 31;
+select count(SC_Grade) as 'Nya betyg denna månad' from tblStudentsCourses ---Should be 2 grades with current table values
+where DATEDIFF(DAY, SC_DateOfGrade, GETDATE()) <= 31;
 
 GO
 
-alter proc spUtvaldPersonal
-@ValdKategori varchar(20)
+create proc spChosenStaff
+@ChosenJob varchar(20)
 with encryption
 as 
-if (@ValdKategori = 'Inget')
+if (@ChosenJob = 'Inget')
 begin
-select P_ID, P_Förnamn, P_Efternamn, P_Arbete  from tblPersonal_
+select E_ID, E_FirstName, E_LastName, E_Job  from tblEmployees
 end
 else
 begin
-select P_ID, P_Förnamn, P_Efternamn, P_Arbete  from tblPersonal_
-where P_Arbete = @ValdKategori 
+select E_ID, E_FirstName, E_LastName, E_Job  from tblEmployees
+where E_Job = @ChosenJob 
 END
 
-drop proc spUtvaldPersonal
-exec spUtvaldPersonal 'SYV'  
+drop proc spChosenStaff
+exec spChosenStaff 'SYV'  
 
 GO
 
-alter view vwMånadensBetyg
+create view vwMonthlyGrades
 as
-select (E_Förnamn + char(9) + E_Efternamn) as 'Elev',  (K_Namn) as 'Kurs', (B_Bokstav) as 'Betyg', EK_BetygDatum from tblEleverKurser
-join tblElever on E_ID = EK_ElevID
-join tblKurser on K_ID = EK_KursID
-join tblBetyg on B_Bokstav = EK_Betyg
-where DATEDIFF(DAY, EK_BetygDatum, GETDATE()) <= 31
+select (S_FirstName + char(9) + S_LastName) as 'Elev',  (C_Name) as 'Kurs', 
+(G_Letter) as 'Betyg', SC_DateOfGrade as 'Datum' from tblStudentsCourses
+join tblStudents on S_ID = SC_StudentID
+join tblCourses on C_ID = SC_CourseID
+join tblGrades on G_Letter = SC_Grade
+where DATEDIFF(DAY, SC_DateOfGrade, GETDATE()) <= 31
 
 GO
 
-
-select * from vwMånadensBetyg
+select * from vwMonthlyGrades
 
 GO
 
-create view vwBetygStatistik
+create view vwGradeStatistics
 as
-select (K_Namn) as Kurs,
+select (C_Name) as Kurs,
 CASE 
-WHEN AVG(B_Värde) = 20 THEN 'A' WHEN AVG(B_Värde) >= 19.25 THEN 'A-'
-WHEN AVG(B_Värde) >= 18.75 THEN 'B+' WHEN AVG(B_Värde) >= 17.5 THEN 'B' WHEN AVG(B_Värde) >= 17 THEN 'B-'
-WHEN AVG(B_Värde) >= 16.5 THEN 'C+' WHEN AVG(B_Värde) >= 15 THEN 'C' WHEN AVG(B_Värde) >= 14.5 THEN 'C-'
-WHEN AVG(B_Värde) >= 13.75 THEN 'D+' WHEN AVG(B_Värde) >= 12.5 THEN 'D' WHEN AVG(B_Värde) >= 12 THEN 'D-'
-WHEN AVG(B_Värde) >= 11.5 THEN 'E+' WHEN AVG(B_Värde) >= 10 THEN 'E'
-WHEN AVG(B_Värde) >= 0 THEN 'F'
+WHEN AVG(G_Value) = 20 THEN 'A' WHEN AVG(G_Value) >= 19.25 THEN 'A-'
+WHEN AVG(G_Value) >= 18.75 THEN 'B+' WHEN AVG(G_Value) >= 17.5 THEN 'B' WHEN AVG(G_Value) >= 17 THEN 'B-'
+WHEN AVG(G_Value) >= 16.5 THEN 'C+' WHEN AVG(G_Value) >= 15 THEN 'C' WHEN AVG(G_Value) >= 14.5 THEN 'C-'
+WHEN AVG(G_Value) >= 13.75 THEN 'D+' WHEN AVG(G_Value) >= 12.5 THEN 'D' WHEN AVG(G_Value) >= 12 THEN 'D-'
+WHEN AVG(G_Value) >= 11.5 THEN 'E+' WHEN AVG(G_Value) >= 10 THEN 'E'
+WHEN AVG(G_Value) >= 0 THEN 'F'
 ELSE '-'
 END AS 'Medelbetyg',
 CASE 
-WHEN MIN(B_Värde) = 20 THEN 'A'
-WHEN MIN(B_Värde) = 17.5 THEN 'B'
-WHEN MIN(B_Värde) = 15 THEN 'C'
-WHEN MIN(B_Värde) = 12.5 THEN 'D'
-WHEN MIN(B_Värde) = 10 THEN 'E'
-WHEN MIN(B_Värde) = 0 THEN 'F'
+WHEN MIN(G_Value) = 20 THEN 'A'
+WHEN MIN(G_Value) = 17.5 THEN 'B'
+WHEN MIN(G_Value) = 15 THEN 'C'
+WHEN MIN(G_Value) = 12.5 THEN 'D'
+WHEN MIN(G_Value) = 10 THEN 'E'
+WHEN MIN(G_Value) = 0 THEN 'F'
 ELSE '-'
 END AS 'Lägsta betyg',
 CASE 
-WHEN MAX(B_Värde) = 20 THEN 'A'
-WHEN MAX(B_Värde) = 17.5 THEN 'B'
-WHEN MAX(B_Värde) = 15 THEN 'C'
-WHEN MAX(B_Värde) = 12.5 THEN 'D'
-WHEN MAX(B_Värde) = 10 THEN 'E'
-WHEN MAX(B_Värde) = 0 THEN 'F'
+WHEN MAX(G_Value) = 20 THEN 'A'
+WHEN MAX(G_Value) = 17.5 THEN 'B'
+WHEN MAX(G_Value) = 15 THEN 'C'
+WHEN MAX(G_Value) = 12.5 THEN 'D'
+WHEN MAX(G_Value) = 10 THEN 'E'
+WHEN MAX(G_Value) = 0 THEN 'F'
 ELSE '-'
-END AS 'Högsta betyg' from tblEleverKurser
-join tblBetyg on B_Bokstav = EK_Betyg or B_Bokstav = null
-join tblKurser on K_ID = EK_KursID
-group by K_Namn
-
-GO
-
-select * from vwBetygStatistik
+END AS 'Högsta betyg' from tblStudentsCourses
+join tblGrades on G_Letter = SC_Grade or G_Letter = null
+join tblCourses on C_ID = SC_CourseID
+group by C_Name
 
 go
 
-create proc spElevsAvslutadeKurser
-@ElevID int
+create view vwValueStatistics
+as
+select (C_Name) as Kurs, Round(AVG(G_Value), 2) as 'Medelbetyg', MIN(G_Value) as 'Lägsta betyg', 
+MAX(G_Value) as 'Högsta betyg' from tblStudentsCourses
+join tblGrades on G_Letter = SC_Grade
+join tblCourses on C_ID = SC_CourseID
+group by C_Name
+
+go
+
+select * from vwGradeStatistics
+select * from vwValueStatistics
+
+
+go
+
+create proc spFinishedCourses
+@StudentID int
 as begin
-select K_Namn as 'Kurs', EK_Betyg as 'Betyg',
-(P_Förnamn + char(9) + P_Efternamn) as 'Lärare', (EK_BetygDatum) as 'Datum'  from tblEleverKurser
-join tblElever on EK_ElevID = E_ID 
-join tblKurser on EK_KursID = K_ID
-join tblKurserPersonal on EK_KursID = KP_KursID
-join  tblPersonal_ on KP_PersonalID = P_ID
-where EK_Betyg != '-' and
-E_ID = 1
-order by K_Namn 
+select distinct C_Name as 'Kurs', SC_Grade as 'Betyg',
+(E_FirstName + char(9) + E_LastName) as 'Lärare', (SC_DateOfGrade) as 'Datum'  from tblStudentsCourses
+join tblStudents on SC_StudentID = S_ID 
+join tblCourses on SC_CourseID = C_ID
+join tblTeachersCourses on SC_CourseID = TC_CourseID
+join  tblEmployees on TC_TeacherID = E_ID
+where SC_Grade != '-' and
+S_ID = 1
+order by C_Name 
 end 
 
-exec spElevsAvslutadeKurser 2
+select distinct C_Name from tblCourses 
 
-select (E_Förnamn + char(9) + E_Efternamn) as 'Elev', K_Namn as 'Kurs', EK_Betyg as 'Betyg' from tblEleverKurser
-join tblElever on EK_ElevID = E_ID
-join tblKurser on EK_KursID = K_ID
-where E_Klass_ID = 5
-order by E_Efternamn asc
+exec spFinispFinishedCourses2
+
+select (S_FirstName + char(9) + S_LastName) as 'Elev', C_Name as 'Kurs', SC_Grade as 'Betyg' from tblStudentsCourses
+join tblStudents on SC_StudentID = S_ID
+join tblCourses on SC_CourseID = C_ID
+where S_ClassID = 5
+order by S_LastName asc
 
 GO
 
-create proc spSkapaElever
-@ElevID int, @ElevFörnamn varchar(20), @ElevEfternamn varchar(20), @ElevPersonnummer varchar(20), @ElevKlassID int
+create proc spAddStudents
+@StudentID int, @StudentFName varchar(20), @StudentLName varchar(20), @StudentSNum varchar(20), @StudentClassID int
 as begin
-insert into tblElever
-(E_ID, E_Förnamn, E_Efternamn, E_Personnummer, E_Klass_ID)
+insert into tblStudents
+(S_ID, S_FirstName, S_LastName, S_SecurityNumber, S_ClassID)
 values
-(@ElevID, @ElevFörnamn, @ElevEfternamn, @ElevPersonnummer, @ElevKlassID)
+(@StudentID, @StudentFName, @StudentLName, @StudentSNum, @StudentClassID)
 END
 
 GO
 
-exec spSkapaElever 121, 'Anders', 'Holm', '20050101-1551', 10
+exec spAddStuspAddStudents 121, 'Anders', 'Holm', '20050101-1551', 10
 
 go
 
-alter view vwPersonalInfo 
+create view vwEmployeesInfo
 as
-select (P_Förnamn + ' '+ P_Efternamn) as 'Personal', P_Arbete as 'Arbete', P_ÅrAvArbete as 'År på skolan' from tblPersonal_
+select (E_FirstName + ' '+ E_LastName) as 'Personal', E_Job as 'Arbete', E_YearsOfWork as 'År på skolan' from tblEmployees
 
 select * from vwPersonalInfo
 
 GO
 
-create proc spSkapaPersonal
-@PersID int, @PersFörnamn varchar(20), @PersEfternamn varchar(20), @PersArbete varchar(20), @PersÅr int, @PersLön int
+create proc spAddStaff
+@EmpID int, @EmpFName varchar(20), @EmpLName varchar(20), @EmpJob varchar(20), @EmpYears int, @EmpSalary int
 as begin
-insert into tblPersonal_
-(P_ID, P_Förnamn, P_Efternamn, P_Arbete, P_ÅrAvArbete, P_Lön)
+insert into tblEmployees
+(E_ID, E_FirstName, E_LastName, E_Job, E_YearsOfWork, E_Salary)
 values
-(@PersID, @PersFörnamn, @PersEfternamn, @PersArbete, @PersÅr, @PersLön)
+(@EmpID, @EmpFName, @EmpLName, @EmpJob, @EmpYears, @EmpSalary)
 END
 
 GO
 
-alter view vwPersonalMedellön
+alter view vwStaffAvgSalary
 as
-select P_Arbete as 'Avdelning', AVG(P_Lön) as 'Medellön' from tblPersonal_
-group by P_Arbete 
+select D_Name as 'Avdelning', AVG(E_Salary) as 'Medellön' from tblTeachers
+join tblDepartment on T_DepartmentID = D_ID
+join tblEmployees on T_ID = E_ID
+group by D_Name 
 
-select Avdelning, Round(Medellön, 0) from vwPersonalMedellön
+select Avdelning, Round(Medellön, 0) from vwStaffAvgSalary
 
 go
 
 
-create proc spEleverInfo
-@ElevID int
+create proc spStudentsInfo
+@StudentID int
 as begin
-select (E_Förnamn + ' ' + E_Efternamn) as 'Elev', E_Personnummer as 'Personnummer' from tblElever
+select (S_FirstName + ' ' + S_LastName) as 'Elev', S_SecurityNumber as 'Personnummer' from tblStudents
 end
 
 go
 
-create proc spBetygSättning
-@ElevID int, @ElevKursID int, @ElevBetyg char
+create proc spGrading
+@StudentID int, @StudentCourseID int, @StudentGrade char
 as 
 begin try
  begin transaction 
-  update tblEleverKurser set EK_Betyg = @ElevBetyg 
-  where EK_ElevID = @ElevID and EK_KursID = @ElevKursID
+  update tblStudentsCourses set SC_Grade = @StudentGrade 
+  where SC_StudentID = @StudentID and SC_CourseID = @StudentCourseID
  commit transaction
 Print 'Betyget har satts!'
 end try
 
+go
 --begin try
 --  begin transaction
 --    update tblPerson set Salary = 45000 where Age > 30
@@ -192,6 +212,23 @@ end try
 --print 'transaction rolled back'
 --end catch
 
+create view vwFinishedCourses
+as
+select C_Name as 'Kurs', SC_Grade as 'Betyg',
+(E_FirstName + char(9) + E_LastName) as 'Lärare', (SC_DateOfGrade) as 'Datum'  from tblStudentsCourses
+join tblStudents on SC_StudentID = S_ID 
+join tblCourses on SC_CourseID = C_ID
+join tblTeachersCourses on SC_CourseID = TC_CourseID
+join  tblEmployees on TC_TeacherID = E_ID
+where SC_Grade != '-' and
+S_ID = 1
+order by C_Name asc
+
+select * from vwFinishedCourses
+select C_Name, E_FirstName, E_LastName from tblTeachersCourses
+join tblCourses on TC_CourseID = C_ID
+join tblEmployees on TC_TeacherID = E_ID
+order by C_Name
 /*Distinct join
 --select a.FirstName, a.LastName, v.District
 --from AddTbl a 
